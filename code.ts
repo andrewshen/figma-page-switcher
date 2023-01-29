b264d4fb9397aba2a4a2e112522d71f7b631e3a7
Andrew Shen <andrewshen@gmail.com>
2023-01-29 14:23:03 -0800

    Add page switching

--- code.ts
+++ code.ts
@@ -1,21 +1,20 @@
-// This plugin creates 5 rectangles on the screen.
-const numberOfRectangles = 5
+figma.parameters.on("input", ({ query, result }: ParameterInputEvent) => {
+  result.setSuggestions(
+    figma.root.children
+      .filter(({ name }) => name.toLowerCase().includes(query.toLowerCase()))
+      .map(({ name, id }) => {
+        return {
+          name: name,
+          data: id,
+        };
+      })
+  );
+});
 
-// This file holds the main code for the plugins. It has access to the *document*.
-// You can access browser APIs such as the network by creating a UI which contains
-// a full browser environment (see documentation).
-
-const nodes: SceneNode[] = [];
-for (let i = 0; i < numberOfRectangles; i++) {
-  const rect = figma.createRectangle();
-  rect.x = i * 150;
-  rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-  figma.currentPage.appendChild(rect);
-  nodes.push(rect);
-}
-figma.currentPage.selection = nodes;
-figma.viewport.scrollAndZoomIntoView(nodes);
-
-// Make sure to close the plugin when you're done. Otherwise the plugin will
-// keep running, which shows the cancel button at the bottom of the screen.
-figma.closePlugin();
+figma.on("run", ({ parameters }: RunEvent) => {
+  const selectedPageId = parameters?.["page"];
+  figma.currentPage = figma.root.children.filter(
+    ({ id }) => id === selectedPageId
+  )[0];
+  figma.closePlugin();
+});
